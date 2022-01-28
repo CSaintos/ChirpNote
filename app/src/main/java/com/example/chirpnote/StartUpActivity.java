b3a55431c88548d8832e1de9fdfb622c7814bea5
CSaintos <48599958+CSaintos@users.noreply.github.com>
2022-01-27 21:08:59 -0800

    Add Transition Animations

--- app/src/main/java/com/example/chirpnote/StartUpActivity.java
+++ app/src/main/java/com/example/chirpnote/StartUpActivity.java
@@ -6,16 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
-import com.blogspot.atifsoftwares.animatoolib.Animatoo;
-
 import io.realm.Realm;
-import io.realm.mongodb.App;
-import io.realm.mongodb.AppConfiguration;
 
 public class StartUpActivity extends AppCompatActivity {
-
-    App app;
-    String appID = "chirpnote-jwrci";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +17,9 @@ public class StartUpActivity extends AppCompatActivity {
 
         Realm.init(this); // initializes realm, only need to do this once
 
-        app = new App(new AppConfiguration.Builder(appID).build());
         Log.d("Success","Start Screen Created");
         startActivity(new Intent(StartUpActivity.this, LoginActivity.class));
-        Animatoo.animateFade(StartUpActivity.this);
+        overridePendingTransition(R.anim.fade_out, R.anim.static_animation);
     }
 
 
