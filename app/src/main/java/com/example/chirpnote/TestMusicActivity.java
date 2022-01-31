d5806840c1eaefe45af5e76b73459123cdfad655
smlgrc <samuel.garcia01@student.csulb.edu>
2022-01-30 18:09:04 -0800

    Implemented keyboard layout and added more notes
    The keyboard layout has aesthetic issues but the buttons play fine.

--- app/src/main/java/com/example/chirpnote/TestMusicActivity.java
+++ app/src/main/java/com/example/chirpnote/TestMusicActivity.java
@@ -35,9 +35,23 @@ public class TestMusicActivity extends AppCompatActivity {
         pianoKeys = new ArrayList<>(); // List of notes
         // You can also create a new MusicNote without a Melody if you just want to test the keyboard playback stuff
         // For example: pianoKeys.add(new MusicNote(59, (Button) findViewById(R.id.noteBButton))
-        pianoKeys.add(new MusicNote(60, (Button) findViewById(R.id.noteCButton), melody));
-        pianoKeys.add(new MusicNote(62, (Button) findViewById(R.id.noteDButton), melody));
-        pianoKeys.add(new MusicNote(64, (Button) findViewById(R.id.noteEButton), melody));
+//        pianoKeys.add(new MusicNote(60, (Button) findViewById(R.id.noteCButton), melody));
+//        pianoKeys.add(new MusicNote(62, (Button) findViewById(R.id.noteDButton), melody));
+//        pianoKeys.add(new MusicNote(64, (Button) findViewById(R.id.noteEButton), melody));
+
+        pianoKeys.add(new MusicNote(60, (Button) findViewById(R.id.noteC4Button), melody));
+        pianoKeys.add(new MusicNote(61, (Button) findViewById(R.id.noteCSharp4Button), melody));
+        pianoKeys.add(new MusicNote(62, (Button) findViewById(R.id.noteD4Button), melody));
+        pianoKeys.add(new MusicNote(63, (Button) findViewById(R.id.noteDSharp4Button), melody));
+        pianoKeys.add(new MusicNote(64, (Button) findViewById(R.id.noteE4Button), melody));
+        pianoKeys.add(new MusicNote(65, (Button) findViewById(R.id.noteF4Button), melody));
+        pianoKeys.add(new MusicNote(66, (Button) findViewById(R.id.noteFSharp4Button), melody));
+        pianoKeys.add(new MusicNote(67, (Button) findViewById(R.id.noteG4Button), melody));
+        pianoKeys.add(new MusicNote(68, (Button) findViewById(R.id.noteGSharp4Button), melody));
+        pianoKeys.add(new MusicNote(69, (Button) findViewById(R.id.noteA4Button), melody));
+        pianoKeys.add(new MusicNote(70, (Button) findViewById(R.id.noteASharp4Button), melody));
+        pianoKeys.add(new MusicNote(71, (Button) findViewById(R.id.noteB4Button), melody));
+        pianoKeys.add(new MusicNote(72, (Button) findViewById(R.id.noteC5Button), melody));
 
         // Event listener for record button (to record melody)
         recordButton.setOnClickListener(new OnClickListener() {
