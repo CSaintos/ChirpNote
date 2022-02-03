49e543832a7aae60a64267832b112254d1892e50
name-goes-here <starkiller127.bh@gmail.com>
2022-02-02 21:15:04 -0800

    Implemented functionality for adding notes to the melody

--- app/src/main/java/com/example/chirpnote/Melody.java
+++ app/src/main/java/com/example/chirpnote/Melody.java
@@ -15,14 +15,16 @@ import com.leff.midi.event.meta.TimeSignature;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Melody {
     // States
     private boolean mRecording;
     private boolean mRecorded;
+    private boolean mBuilding;
     private boolean mMelodyRewritten;
 
-    // For recording
+    // For recording the melody (in real time)
     public final int RESOLUTION = 960;
     private int mBPM;
     private MidiTrack mTempoTrack;
@@ -30,6 +32,10 @@ public class Melody {
     private File mOutput;
     private String mFilePath;
     private long mRecordingStartTime;
+
+    // For building the melody (one note at a time)
+    private HashSet<Integer> mNoteDurations;
+    private int mPrevTick;
 
     // For playback
     private MediaPlayer mMediaPlayer;
@@ -43,6 +49,7 @@ public class Melody {
     public Melody(int tempo, String filePath, Button playButton){
         mRecording = false;
         mRecorded = false;
+        mBuilding = false;
         mMelodyRewritten = false;
         mMediaPlayer = new MediaPlayer();
         mMediaPlayer.setOnPreparedListener(new OnPreparedListener() {
@@ -61,7 +68,16 @@ public class Melody {
         mBPM = tempo;
         mFilePath = filePath;
         mPlayButton = playButton;
-        mPlayButton.setEnabled(false);
+
+        // Possible note durations when building a melody
+        mNoteDurations = new HashSet<>();
+        mNoteDurations.add(1); // Whole note
+        mNoteDurations.add(2); // Half note
+        mNoteDurations.add(4); // Quarter note
+        mNoteDurations.add(8); // Eighth note
+        mNoteDurations.add(16); // Sixteenth note
+
+        mPrevTick = 0;
     }
 
     /**
@@ -74,7 +90,7 @@ public class Melody {
 
     /**
      * Gets whether or not the melody is currently being recorded
-     * @return True if the melody is being played
+     * @return True if the melody is being recorded in real time
      */
     public boolean isRecording(){
         return mRecording;
@@ -89,22 +105,28 @@ public class Melody {
     }
 
     /**
+     * Gets whether or not the melody is currently being built
+     * @return True if the melody is being built by adding notes
+     */
+    public boolean isBuilding(){
+        return mBuilding;
+    }
+
+    /**
      * Starts the recording process for this MIDI melody
      * @return False if melody is already being recorded (cannot start a new recording process until the current one is stopped)
      */
     public boolean startRecording(){
-        if(mRecording){
+        if(mRecording || mBuilding || mMediaPlayer.isPlaying()){
             return false;
         }
         mRecording = true;
-        mPlayButton.setEnabled(false);
         mRecordingStartTime = System.currentTimeMillis();
 
         // Setup MIDI tracks
         mTempoTrack = new MidiTrack();
         mNoteTrack = new MidiTrack();
 
-        // Time signature setup (Hard coding 4/4...should be obtained from session settings)
         TimeSignature ts = new TimeSignature();
         ts.setTimeSignature(4, 4, TimeSignature.DEFAULT_METER, TimeSignature.DEFAULT_DIVISION);
         mTempoTrack.insertEvent(ts);
@@ -118,16 +140,6 @@ public class Melody {
     }
 
     /**
-     * Adds a note to this melody
-     * (Will write this method later, but will be used for when we "record" a melody by just letting the user select notes to add)
-     * @param note The note to add
-     * @return False because this method does nothing yet
-     */
-    public boolean addNote(MusicNote note){
-        return false;
-    }
-
-    /**
      * Writes a note on event for the given note to this melody. Should be called when a note is stopped.
      * (Note: Do not call, unless you manually want to add a noteOn event.
      * This happens automatically during an active recording process, if the note has been constructed with a melody)
@@ -135,7 +147,7 @@ public class Melody {
      * @return False if melody is not being recorded yet (cannot add a note if there is no active recording process)
      */
     public boolean writeNoteOn(MusicNote note){
-        if(!mRecording || note == null){
+        if(!mRecording || mBuilding || note == null){
             return false;
         }
         mNoteTrack.insertEvent(new NoteOn(getCurrentTick(), 0, note.getNoteNumber(), note.VELOCITY));
@@ -150,7 +162,7 @@ public class Melody {
      * @return False if melody is not being recorded, or if the given note is null
      */
     public boolean writeNoteOff(MusicNote note){
-        if(!mRecording || note == null){
+        if(!mRecording || mBuilding || note == null){
             return false;
         }
         mNoteTrack.insertEvent(new NoteOff(getCurrentTick(), 0, note.getNoteNumber(), note.VELOCITY));
@@ -176,18 +188,84 @@ public class Melody {
         // Write tracks to MIDI file
         MidiFile midi = new MidiFile(RESOLUTION, tracks);
         mOutput = new File(mFilePath);
-        try
-        {
+        try {
             midi.writeToFile(mOutput);
-        }
-        catch(IOException e)
-        {
+        } catch(IOException e) {
             System.err.println(e);
         }
         mRecording = false;
         mRecorded = true;
         mMelodyRewritten = true;
-        mPlayButton.setEnabled(true);
+        return true;
+    }
+
+    /**
+     * Starts the building process for this MIDI melody
+     * @return False if melody is already being built (cannot start a new building process until the current one is stopped)
+     */
+    public boolean startBuilding(){
+        if(mBuilding || mRecording || mMediaPlayer.isPlaying()){
+            return false;
+        }
+        mBuilding = true;
+
+        // Setup MIDI tracks
+        mTempoTrack = new MidiTrack();
+        mNoteTrack = new MidiTrack();
+
+        TimeSignature ts = new TimeSignature();
+        ts.setTimeSignature(4, 4, TimeSignature.DEFAULT_METER, TimeSignature.DEFAULT_DIVISION);
+        mTempoTrack.insertEvent(ts);
+
+        // Tempo setup
+        Tempo tempo = new Tempo();
+        tempo.setBpm(mBPM);
+        mTempoTrack.insertEvent(tempo);
+
+        return true;
+    }
+
+    /**
+     * Adds a note to this melody with the given duration. Used to add notes
+     * when "building" a melody, instead of playing it in real time
+     * @param note The note to add
+     * @param duration The note duration (1 = whole note, 2 = half, 4 = quarter, etc...)
+     * @return False if not currently building a melody, or the note duration is invalid
+     */
+    public boolean addNote(MusicNote note, int duration){
+        if(!mBuilding || mRecording || note == null || !mNoteDurations.contains(duration)){
+            return false;
+        }
+        int noteDuration = RESOLUTION * 4 / duration;
+        mNoteTrack.insertNote(0, note.getNoteNumber(), note.VELOCITY, mPrevTick, noteDuration);
+        mPrevTick += noteDuration;
+        return true;
+    }
+
+    /**
+     * Stops the building process for this MIDI melody
+     * @return False if melody is not being built yet (cannot stop a building process if it has not been started yet)
+     */
+    public boolean stopBuilding(){
+        if(!mBuilding){
+            return false;
+        }
+        ArrayList<MidiTrack> tracks = new ArrayList<>();
+        tracks.add(mTempoTrack);
+        tracks.add(mNoteTrack);
+
+        // Write tracks to MIDI file
+        MidiFile midi = new MidiFile(RESOLUTION, tracks);
+        mOutput = new File(mFilePath);
+        try {
+            midi.writeToFile(mOutput);
+        } catch(IOException e) {
+            System.err.println(e);
+        }
+        mBuilding = false;
+        mRecorded = true;
+        mMelodyRewritten = true;
+        mPrevTick = 0;
         return true;
     }
 
--- app/src/main/java/com/example/chirpnote/activities/TestOtherActivity.java
+++ app/src/main/java/com/example/chirpnote/activities/TestOtherActivity.java
@@ -8,6 +8,9 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.ToggleButton;
 
 import com.example.chirpnote.Melody;
 import com.example.chirpnote.MusicNote;
@@ -21,6 +24,8 @@ public class TestOtherActivity extends AppCompatActivity {
 
     private MidiDriver midiDriver;
     private ArrayList<MusicNote> pianoKeys;
+    private ToggleButton[] noteTypes = new ToggleButton[4];
+    private int toggledNoteType = 0;
     Melody melody;
     boolean recordingAudio = false;
 
@@ -29,6 +34,7 @@ public class TestOtherActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_other);
 
+        Button addNotesButton = (Button) findViewById(R.id.addNotesButton);
         Button recordMelodyButton = (Button) findViewById(R.id.recordMelodyButton);
         Button recordAudioButton = (Button) findViewById(R.id.recordAudioButton);
         Button playButton = (Button) findViewById(R.id.playButton);
@@ -41,7 +47,9 @@ public class TestOtherActivity extends AppCompatActivity {
         // You can also create a new MusicNote without a Melody if you just want to test the keyboard playback stuff
         // For example: pianoKeys.add(new MusicNote(59, (Button) findViewById(R.id.noteBButton))
         pianoKeys.add(new MusicNote(60, (Button) findViewById(R.id.noteCButton), melody));
+        pianoKeys.add(new MusicNote(61, (Button) findViewById(R.id.noteCSharpButton), melody));
         pianoKeys.add(new MusicNote(62, (Button) findViewById(R.id.noteDButton), melody));
+        pianoKeys.add(new MusicNote(63, (Button) findViewById(R.id.noteDSharpButton), melody));
         pianoKeys.add(new MusicNote(64, (Button) findViewById(R.id.noteEButton), melody));
 
         // Setup event listener for each piano key
@@ -51,6 +59,7 @@ public class TestOtherActivity extends AppCompatActivity {
                 public boolean onTouch(View v, MotionEvent event) {
                     if(event.getAction() == MotionEvent.ACTION_DOWN) {
                         note.playNote(midiDriver);
+                        melody.addNote(note, (int) Math.pow(2, toggledNoteType));
                     } else if (event.getAction() == MotionEvent.ACTION_UP) {
                         note.stopNote(midiDriver);
                     }
@@ -59,12 +68,58 @@ public class TestOtherActivity extends AppCompatActivity {
             });
         }
 
-        // Event listener for record melody button (to record a melody with MIDI notes (the piano keys))
+        // Toggle buttons to select the type/duration of note you want to add to the melody
+        noteTypes[0] = (ToggleButton) findViewById(R.id.wholeToggleButton);
+        noteTypes[1] = (ToggleButton) findViewById(R.id.halfToggleButton);
+        noteTypes[2] = (ToggleButton) findViewById(R.id.quarterToggleButton);
+        noteTypes[3] = (ToggleButton) findViewById(R.id.eighthToggleButton);
+        for(ToggleButton tb : noteTypes){
+            tb.setEnabled(false);
+        }
+
+        // Setup event listener for each note type toggle
+        for(int i = 0; i < noteTypes.length; i++){
+            int current = i;
+            noteTypes[i].setOnCheckedChangeListener(new OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if(isChecked){
+                        noteTypes[toggledNoteType].setChecked(false);
+                        toggledNoteType = current;
+                    }
+                }
+            });
+        }
+
+        // Event listener for add notes button (to build a melody by adding notes one at a time)
+        addNotesButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                recordMelodyButton.setEnabled(melody.isBuilding());
+                recordAudioButton.setEnabled(melody.isBuilding());
+                playButton.setEnabled(melody.isBuilding());
+                if(!melody.isBuilding()){
+                    addNotesButton.setText("Stop Adding Notes");
+                    melody.startBuilding();
+                } else {
+                    addNotesButton.setText("Add Notes to Melody");
+                    melody.stopBuilding();
+                }
+                for(ToggleButton tb : noteTypes){
+                    tb.setEnabled(melody.isBuilding());
+                }
+            }
+        });
+
+        // Event listener for record melody button (to record a melody in real time with the piano keys)
         recordMelodyButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
+                addNotesButton.setEnabled(melody.isRecording());
+                recordAudioButton.setEnabled(melody.isRecording());
+                playButton.setEnabled(melody.isRecording());
                 if(!melody.isRecording()){
-                    recordMelodyButton.setText("End recording");
+                    recordMelodyButton.setText("End Recording");
                     melody.startRecording();
                 } else {
                     recordMelodyButton.setText("Record Melody");
@@ -77,8 +132,11 @@ public class TestOtherActivity extends AppCompatActivity {
         recordAudioButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
+                addNotesButton.setEnabled(recordingAudio);
+                recordMelodyButton.setEnabled(recordingAudio);
+                playButton.setEnabled(recordingAudio);
                 if(!recordingAudio){
-                    recordAudioButton.setText("End recording");
+                    recordAudioButton.setText("End Recording");
                 } else {
                     recordAudioButton.setText("Record Audio");
                 }
