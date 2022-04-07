2a11ed50bf6e965c9407d070b73b5deab321f3e9
CSaintos <48599958+CSaintos@users.noreply.github.com>
2022-04-06 22:37:43 -0700

    Implemented Percussion UI selector buttons

--- app/src/main/java/com/example/chirpnote/activities/PercussionActivity.java
+++ app/src/main/java/com/example/chirpnote/activities/PercussionActivity.java
@@ -1,15 +1,21 @@
 package com.example.chirpnote.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintSet;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.graphics.PorterDuff;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.ArrayMap;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TableLayout;
@@ -25,21 +31,25 @@ import java.util.ArrayList;
 
 public class PercussionActivity extends AppCompatActivity {
 
+    private ArrayMap<RadioButton, ArrayList<RadioButton>> chordButtons;
     private ArrayList<ArrayList<RadioButton>> patternList;
     private ArrayList<RadioButton> styleList;
     //private ArrayList<TableRow> tableRows;
     private String[] styles;
 
     //private TableLayout tableLayout;
+    private LinearLayout chordLayout;
     private RadioGroup chordGroup;
     private RadioGroup styleGroup;
     private RadioGroup patternGroup;
-    private Button backButton;
+    //private Button backButton;
     private Button leftButton;
     private Button rightButton;
 
     // The driver that allows us to play MIDI notes
     private MidiDriver midiDriver;
+
+    private int chordButtonId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,14 +63,18 @@ public class PercussionActivity extends AppCompatActivity {
         //tableRows = new ArrayList<>();
         patternList = new ArrayList<>();
         styleList = new ArrayList<>();
+        chordButtons = new ArrayMap<>();
         styles = new String[]{"Pop", "Rock"};
 
         chordGroup = findViewById(R.id.percussionChordGroup);
         styleGroup = findViewById(R.id.percussionStyleGroup);
         patternGroup = findViewById(R.id.percussionPatternGroup);
-        backButton = (Button) findViewById(R.id.percussionbackbutton);
+        //backButton = (Button) findViewById(R.id.percussionbackbutton);
         leftButton = (Button) findViewById(R.id.percussionLeftButton);
         rightButton = (Button) findViewById(R.id.percussionRightButton);
+        chordLayout = (findViewById(R.id.percussionChordLayout));
+
+        chordButtonId = 69;
         /*
         tableLayout = findViewById(R.id.percussionTableLayout);
         for (int i = 0; i < tableLayout.getChildCount(); i++) {
@@ -70,17 +84,19 @@ public class PercussionActivity extends AppCompatActivity {
         chordGroup.removeAllViews();
         styleGroup.removeAllViews();
         patternGroup.removeAllViews();
+        chordLayout.removeAllViews();
 
         initStyles();
-        //displayChords();
+        displayChords();
 
+        /*
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //startActivity(new Intent(PercussionActivity.this, SessionActivity.class));
                 startActivity(new Intent(PercussionActivity.this, HomeScreenActivity.class));
             }
-        });
+        });*/
 
         leftButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -107,15 +123,15 @@ public class PercussionActivity extends AppCompatActivity {
             rb.setBackground(getDrawable(R.drawable.radio_normal));
 
             rb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-               @Override
-               public void onCheckedChanged(CompoundButton cb, boolean isChecked) {
-                   if (isChecked) {
-                       cb.setBackground(getDrawable(R.drawable.radio_selected));
-                       initPatterns(str);
-                   } else {
-                       cb.setBackground(getDrawable(R.drawable.radio_normal));
-                   }
-               }
+                @Override
+                public void onCheckedChanged(CompoundButton cb, boolean isChecked) {
+                    if (isChecked) {
+                        cb.setBackground(getDrawable(R.drawable.radio_selected));
+                        initPatterns(str);
+                    } else {
+                        cb.setBackground(getDrawable(R.drawable.radio_normal));
+                    }
+                }
             });
 
             styleGroup.addView(rb);
@@ -157,6 +173,109 @@ public class PercussionActivity extends AppCompatActivity {
 
         // Add radio button to radio group
         patternGroup.addView(rb);
+    }
+
+    void displayChords() {
+
+        for (int i = 0; i < 4; i++) {
+            addRow();
+        }
+    }
+
+    void addRow() {
+        LinearLayout row = new LinearLayout(this);
+
+        row.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        row.setOrientation(LinearLayout.HORIZONTAL);
+        row.setDividerPadding(10);
+
+
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(150, 80);
+
+        // Children of row Linear Layout
+        RadioButton rb = new RadioButton(this);
+        layoutParams.setMargins(70, 10, 70, 10);
+        rb.setLayoutParams(layoutParams);
+        rb.setId(chordButtonId);
+        chordButtonId++;
+
+        rb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton cb, boolean isChecked) {
+                if (isChecked) {
+                    ArrayList<RadioButton> tempButtons = chordButtons.get(cb);
+                    for (int i = 0; i < tempButtons.size(); i++) {
+                        RadioButton temp = tempButtons.get(i);
+                        temp.setChecked(true);
+                        temp.setBackground(getDrawable(R.drawable.radio_selected));
+                    }
+                } else {
+
+                }
+            }
+        });
+
+        chordButtons.put(rb, new ArrayList<>());
+        row.addView(rb);
+
+        layoutParams.setMargins(20, 10, 20, 10);
+
+        for (int i = 0; i < 4; i++) {
+            RadioButton rb0 = new RadioButton(this);
+            rb0.setLayoutParams(layoutParams);
+            rb0.setBackground(getDrawable(R.drawable.radio_selector));
+            rb0.setText("I");
+            rb0.setId(chordButtonId);
+            chordButtonId++;
+
+            rb0.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    CompoundButton cb = (CompoundButton) v;
+                    cb.setChecked(true);
+                    cb.setBackground(getDrawable(R.drawable.radio_selected));
+
+                    for (int i = 0; i < chordButtons.size(); i++) {
+                        for (int j = 0; j < chordButtons.valueAt(i).size(); j++) {
+                            RadioButton temp = chordButtons.valueAt(i).get(j);
+                            if (cb.getId() != temp.getId()) {
+                                temp.setChecked(false);
+                                temp.setBackground(getDrawable(R.drawable.radio_normal));
+                                //Log.d("RadioButton", (String) (temp.getId() + "turned off"));
+                            }
+                        }
+                        chordButtons.keyAt(i).setChecked(false);
+                    }
+                }
+            });
+
+            /*
+            rb0.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton cb, boolean isChecked) {
+                    if (isChecked) {
+                        cb.setBackground(getDrawable(R.drawable.radio_selected));
+                        for (int i = 0; i < chordButtons.size(); i++) {
+                            for (int j = 0; j < chordButtons.valueAt(i).size(); j++) {
+                                RadioButton temp = chordButtons.valueAt(i).get(j);
+                                if (cb.getId() != temp.getId()) {
+                                    temp.setChecked(false);
+                                    temp.setBackground(getDrawable(R.drawable.radio_normal));
+                                }
+                            }
+                            chordButtons.keyAt(i).setChecked(false);
+                        }
+                    } else {
+                        cb.setBackground(getDrawable(R.drawable.radio_normal));
+                    }
+                }
+            });*/
+
+            chordButtons.get(rb).add(rb0);
+            row.addView(rb0);
+        }
+
+        chordLayout.addView(row);
     }
 
     /*
