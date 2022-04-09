78af0a7d99f811440e8d735ccc592da019d3fc1a
EdwinH7 <45644797+EdwinH7@users.noreply.github.com>
2022-04-09 00:38:59 -0700

    tidied up drive export method. Now functioning.

--- app/src/main/java/com/example/chirpnote/ExportHelper.java
+++ app/src/main/java/com/example/chirpnote/ExportHelper.java
@@ -2,6 +2,7 @@ package com.example.chirpnote;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Environment;
 
@@ -11,6 +12,7 @@ import net.lingala.zip4j.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
 
 import java.io.File;
+import java.util.List;
 
 public class ExportHelper {
     public ExportHelper(){}
@@ -40,5 +42,32 @@ public class ExportHelper {
         intent.putExtra(Intent.EXTRA_STREAM,audioURI);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(Intent.createChooser(intent, "Share File:"));
+    }
+    public void exportToDrive(Context context, File sharedFile) {
+        boolean found = false;
+        Intent share = new Intent(Intent.ACTION_SEND);
+        share.setType("*/*");
+        Uri aURI = FileProvider.getUriForFile(context, BuildConfig.APPLICATION_ID+ ".provider",sharedFile);
+        // gets the list of intents that can be loaded.
+        List<ResolveInfo> resInfo = context.getPackageManager().queryIntentActivities(share, 0);
+        for (ResolveInfo res:resInfo)
+        System.out.println(res.activityInfo.packageName);
+        if (!resInfo.isEmpty()) {
+            for (ResolveInfo info : resInfo) {
+                if (info.activityInfo.packageName.toLowerCase().contains("com.google.android.apps.docs") ||
+                        info.activityInfo.name.toLowerCase().contains("com.google.android.apps.docs")) {
+                    share.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                    share.putExtra(Intent.EXTRA_STREAM,aURI);
+                    share.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    share.setPackage(info.activityInfo.packageName);
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+                return;
+
+            context.startActivity(Intent.createChooser(share, "Export to Drive"));
+        }
     }
 }
