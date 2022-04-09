ce5e7fd09cfbb32e9e715d05a30baba09c7c3174
EdwinH7 <45644797+EdwinH7@users.noreply.github.com>
2022-04-08 23:59:47 -0700

    Zip and share file method moved to a class added as a method instead for more flexible use.

--- app/src/main/java/com/example/chirpnote/activities/DirectoryPopActivity.java
+++ app/src/main/java/com/example/chirpnote/activities/DirectoryPopActivity.java
@@ -18,6 +18,7 @@ import android.widget.ListView;
 import android.widget.PopupMenu;
 
 import com.example.chirpnote.BuildConfig;
+import com.example.chirpnote.ExportHelper;
 import com.example.chirpnote.R;
 
 import androidx.annotation.Nullable;
@@ -65,7 +66,8 @@ public class DirectoryPopActivity extends Activity {
                     public boolean onMenuItemClick(MenuItem item) {
                         switch (item.getItemId()){
                             case R.id.item_share:
-                                shareFile(exportFile);
+                                ExportHelper eh = new ExportHelper();
+                                eh.shareFile(DirectoryPopActivity.this,exportFile);
                                 return true;
                             case R.id.item_preview_audio:
                                 System.out.println(exportFile.getName());
@@ -74,11 +76,6 @@ public class DirectoryPopActivity extends Activity {
 //                                } catch (IOException e) {
 //                                    e.printStackTrace();
 //                                }
-                                try {
-                                    new ZipFile(getFilesDir() + "/audio.zip").addFolder(new File(getFilesDir() + "/Session/"));
-                                } catch (ZipException e) {
-                                    e.printStackTrace();
-                                }
 
                                 return true;
                             default:
@@ -112,15 +109,6 @@ public class DirectoryPopActivity extends Activity {
                 }
             }
         }
-    }
-    public void shareFile(File sharedFile){
-        Intent intent = new Intent(Intent.ACTION_SEND);
-        Uri audioURI = FileProvider.getUriForFile(context, BuildConfig.APPLICATION_ID+ ".provider",sharedFile);
-        intent.setType("*/*");
-        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-        intent.putExtra(Intent.EXTRA_STREAM,audioURI);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(Intent.createChooser(intent, "Share File:"));
     }
 
 
