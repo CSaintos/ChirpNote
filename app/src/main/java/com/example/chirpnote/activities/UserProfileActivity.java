339175483d619acebcb06a611a03a5187a40eae4
banhmic <michael.banh@student.csulb.edu>
2022-04-12 19:54:27 -0700

    Added hamburger menu to profile activity for testing Added back button for music theory activity

--- app/src/main/java/com/example/chirpnote/activities/UserProfileActivity.java
+++ app/src/main/java/com/example/chirpnote/activities/UserProfileActivity.java
@@ -1,10 +1,18 @@
 package com.example.chirpnote.activities;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.widget.ToolbarWidgetWrapper;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -16,20 +24,61 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.chirpnote.R;
+import io.realm.mongodb.App;
+import io.realm.mongodb.AppConfiguration;
+import io.realm.mongodb.Credentials;
+import io.realm.mongodb.User;
+import io.realm.mongodb.functions.Functions;
+import io.realm.mongodb.mongo.MongoClient;
+import io.realm.mongodb.mongo.MongoCollection;
+import io.realm.mongodb.mongo.MongoDatabase;
 
-public class UserProfileActivity extends AppCompatActivity{
+import com.example.chirpnote.R;
+import com.google.android.material.navigation.NavigationView;
+
+import org.w3c.dom.Document;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public class UserProfileActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
+    Activity context;
+
+    MongoClient mongoClient;
+    MongoDatabase mongoDatabase;
+    MongoCollection<Document> mongoCollection;
+    App app;
+    String appID = "chirpnote-jwrci";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        Toolbar toolbar = findViewById(R.id.nav_toolbar);
+        setSupportActionBar(toolbar);
 
-         /*
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        //navigationView.setCheckedItem(R.id.nav_profile);
+
+        /*
+        String username, password;
+        AtomicReference<User> user = new AtomicReference<>();
+        Context context = getApplicationContext();
+        app = new App(new AppConfiguration.Builder(appID).build());
+        Credentials customFunctionCredentials =
+                Credentials.customFunction(new org.bson.Document("username", username).append("password", password));
         TextView displayCurrentName = (TextView) findViewById(R.id.currentNameText);
-        displayCurrentName.setText(name);
+        displayCurrentName.setText(username);
 
         EditText editNewName = (EditText) findViewById(R.id.editNewName);
 
@@ -40,7 +89,7 @@ public class UserProfileActivity extends AppCompatActivity{
                 String email = editNewName.getText().toString();
 
             }
-        }
+        });
 
         TextView displayCurrentEmail = (TextView) findViewById(R.id.currentEmailText);
         displayCurrentEmail.setText(username);
@@ -54,9 +103,44 @@ public class UserProfileActivity extends AppCompatActivity{
                 String email = editNewEmail.getText().toString();
 
             }
-        });
-          */
+        });*/
+    }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                UserProfileActivity.redirectActivity(this, HomeScreenActivity.class);
+                //Toast.makeText(this, "Home Screen", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_profile:
+                //UserProfileActivity.redirectActivity(this, UserProfileActivity.class);
+                Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_overview:
+                Toast.makeText(this, "Overview", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_melody:
+                //UserProfileActivity.redirectActivity(this, MelodyActivity.class);
+                Toast.makeText(this, "Melody", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
     }
 
     @Override
@@ -81,13 +165,38 @@ public class UserProfileActivity extends AppCompatActivity{
                 break;
             case R.id.subitem3: //subitem 3 = use system preference
                 //boolean darkMode = getDarkModeStatus();
-
-
-                Toast.makeText(this, "Using System Preferences", Toast.LENGTH_SHORT).show();
+                if(getDarkModeStatus()) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    Toast.makeText(this, "Light Mode Enabled", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    Toast.makeText(this, "Dark Mode Enabled", Toast.LENGTH_SHORT).show();
+                }
                 break;
             default:
                 return super.onOptionsItemSelected(item);
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private boolean getDarkModeStatus() {
+        int nightModeFlags=this.getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+
+        switch (nightModeFlags) {
+            case Configuration.UI_MODE_NIGHT_YES:
+                return true;
+            case Configuration.UI_MODE_NIGHT_NO:
+            case Configuration.UI_MODE_NIGHT_UNDEFINED:
+                return false;
+        }
+        return false;
+    }
+
+    private static void redirectActivity(Activity activity, Class aClass) {
+
+        Intent intent = new Intent(activity, aClass);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        activity.startActivity(intent);
     }
 }
