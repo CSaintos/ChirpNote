59fb95e2c1192bd0baf19b83c9f825ccbd6f7543
EdwinH7 <45644797+EdwinH7@users.noreply.github.com>
2022-04-16 01:13:37 -0700

    mixer xml styling correction, functionality fix.

--- app/src/main/java/com/example/chirpnote/activities/MixerActivity.java
+++ app/src/main/java/com/example/chirpnote/activities/MixerActivity.java
@@ -1,21 +1,28 @@
 package com.example.chirpnote.activities;
 import com.example.chirpnote.Mixer;
 import com.example.chirpnote.R;
-import com.google.android.material.slider.RangeSlider;
+import com.google.android.material.slider.Slider;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MixerActivity extends AppCompatActivity {
-    private Mixer mixer;
-    private RangeSlider trackSlider;
+    private Slider trackSlider;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mixer);
         trackSlider = findViewById(R.id.Slider);
+        trackSlider.addOnChangeListener(new Slider.OnChangeListener() {
+            @Override
+            public void onValueChange(@NonNull Slider slider, float value, boolean fromUser) {
+                System.out.println(value);
+            }
+        });
     }
+
 }
