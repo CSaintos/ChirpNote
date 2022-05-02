e31c6ee5867e6308f573632cc63c0d2cbf6293b5
smlgrc <71049193+smlgrc@users.noreply.github.com>
2022-05-01 21:55:32 -0700

    updated the MixerActivity and activity_mixer

--- app/src/main/java/com/example/chirpnote/activities/MixerActivity.java
+++ app/src/main/java/com/example/chirpnote/activities/MixerActivity.java
@@ -1,28 +1,64 @@
 package com.example.chirpnote.activities;
-import com.example.chirpnote.Mixer;
-import com.example.chirpnote.R;
-import com.google.android.material.slider.Slider;
-
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.chirpnote.R;
+import com.google.android.material.slider.Slider;
+
 public class MixerActivity extends AppCompatActivity {
-    private Slider trackSlider;
+    private Slider chordsVolumeSlider;
+    private Slider composedMelodyVolumeSlider;
+    private Slider recordedMelodyVolumeSlider;
+    private Slider audioVolumeSlider;
+    private Slider percussionVolumeSlider;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mixer);
-        trackSlider = findViewById(R.id.Slider);
-        trackSlider.addOnChangeListener(new Slider.OnChangeListener() {
+
+        chordsVolumeSlider = findViewById(R.id.chordsVolumeSlider);
+        chordsVolumeSlider.addOnChangeListener(new Slider.OnChangeListener() {
+            @Override
+            public void onValueChange(@NonNull Slider slider, float value, boolean fromUser) {
+                System.out.println(value);
+
+            }
+        });
+
+        composedMelodyVolumeSlider = findViewById(R.id.composedMelodyVolumeSlider);
+        composedMelodyVolumeSlider.addOnChangeListener(new Slider.OnChangeListener() {
+            @Override
+            public void onValueChange(@NonNull Slider slider, float value, boolean fromUser) {
+                System.out.println(value);
+            }
+        });
+
+        recordedMelodyVolumeSlider = findViewById(R.id.recordedMelodyVolumeSlider);
+        recordedMelodyVolumeSlider.addOnChangeListener(new Slider.OnChangeListener() {
+            @Override
+            public void onValueChange(@NonNull Slider slider, float value, boolean fromUser) {
+                System.out.println(value);
+            }
+        });
+
+        audioVolumeSlider = findViewById(R.id.audioVolumeSlider);
+        audioVolumeSlider.addOnChangeListener(new Slider.OnChangeListener() {
+            @Override
+            public void onValueChange(@NonNull Slider slider, float value, boolean fromUser) {
+                System.out.println(value);
+            }
+        });
+
+        percussionVolumeSlider = findViewById(R.id.percussionVolumeSlider);
+        percussionVolumeSlider.addOnChangeListener(new Slider.OnChangeListener() {
             @Override
             public void onValueChange(@NonNull Slider slider, float value, boolean fromUser) {
                 System.out.println(value);
             }
         });
     }
-
 }
