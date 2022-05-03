7d18e2bdef69253a7a4c7b863a49314e85031fbf
name-goes-here <starkiller127.bh@gmail.com>
2022-05-02 17:47:36 -0700

    Add navigation drawer to Mixer activity

--- app/src/main/java/com/example/chirpnote/activities/MixerActivity.java
+++ app/src/main/java/com/example/chirpnote/activities/MixerActivity.java
@@ -1,24 +1,54 @@
 package com.example.chirpnote.activities;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.chirpnote.R;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.slider.Slider;
 
-public class MixerActivity extends AppCompatActivity {
+import io.realm.mongodb.App;
+import io.realm.mongodb.AppConfiguration;
+
+public class MixerActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private Slider chordsVolumeSlider;
     private Slider composedMelodyVolumeSlider;
     private Slider recordedMelodyVolumeSlider;
     private Slider audioVolumeSlider;
     private Slider percussionVolumeSlider;
+    private DrawerLayout drawer;
+
+    App app;
+    String appID = "chirpnote-jwrci";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mixer);
+
+        app = new App(new AppConfiguration.Builder(appID).build());
+
+        Toolbar toolbar = findViewById(R.id.nav_toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.bringToFront();
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
         chordsVolumeSlider = findViewById(R.id.chordsVolumeSlider);
         chordsVolumeSlider.addOnChangeListener(new Slider.OnChangeListener() {
@@ -60,5 +90,56 @@ public class MixerActivity extends AppCompatActivity {
                 System.out.println(value);
             }
         });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                redirectActivity(this, HomeScreenActivity.class);
+                break;
+            case R.id.nav_overview:
+                redirectActivity(this, SessionOverviewActivity.class);
+                break;
+            case R.id.nav_melody:
+                redirectActivity(this, MelodyActivity.class);
+                break;
+            case R.id.nav_chords:
+                redirectActivity(this, InsertChordsActivity.class);
+                break;
+            case R.id.nav_percussion:
+                redirectActivity(this, PercussionActivity.class);
+                break;
+            case R.id.nav_keyboard:
+                redirectActivity(this, KeyboardActivity.class);
+                break;
+            case R.id.nav_mixer:
+                // Just close the drawer since we're already on this activity
+                drawer.closeDrawer(GravityCompat.START);
+                break;
+            case R.id.nav_audio:
+                redirectActivity(this, RecordAudioActivity.class);
+                break;
+            default:
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+
+    private static void redirectActivity(Activity activity, Class aClass) {
+        Intent intent = new Intent(activity, aClass);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        activity.startActivity(intent);
     }
 }
